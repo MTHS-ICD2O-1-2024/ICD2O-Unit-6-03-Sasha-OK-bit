d680ed4dfbcc398a6b189689c3f6a0a102adeb80
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-05-20 17:07:34 +0000

    Maybe

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@
     })
 }
 
-async function getWeatherData() {
+async function getdata() {
   try {
     const resultJSON = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
     const jsonData = await resultJSON.json()
