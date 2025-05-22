e15fd79e47e22f942bbd8ad0d35cf08dc872bc89
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-05-22 16:55:44 +0000

    Fixed

--- js/script.js
+++ js/script.js
@@ -16,8 +16,8 @@
 async function getData() {
   try {
     const infoJSON = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
-    const kelvinTemp = await infoJSON.main[1]
-    const weatherImage = await infoJSON. weather[4]
+    const kelvinTemp = await infoJSON.main.temp[1]
+    const weatherImage = await infoJSON.weather.icon[4]
     console.log(jsonData)
     const celsiusTemp = kelvinTemp + 273.75
 
